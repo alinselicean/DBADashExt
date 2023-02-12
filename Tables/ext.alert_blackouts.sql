21a9db7497c337a0f62c5c6c1ffb7458d45eb506
Alin Selicean <alin.selicean@gmail.com>
2023-02-12 23:13:01 +0200

    Update ext.alert_blackouts.sql
    refactored the table

--- Tables/ext.alert_blackouts.sql
+++ Tables/ext.alert_blackouts.sql
@@ -6,10 +6,17 @@ begin
 	create table [ext].[alert_blackouts]
 	(	[blackout_id] [int] identity(1,1) not null,
 		[alert_id] [int] not null,
-		[blackout_schedule] varchar(64) not null,
+		[day_of_week] tinyint not null constraint [ck_alert_blackouts_day_of_week] check ([day_of_week] between 1 and 7),
+		[start_time] time not null,
+		[end_time] time not null constraint [ck_alert_blackouts_end_time] check ([end_time] > [start_time]),
+		[start_h] as datepart(hour, [start_time]) persisted,
+		[start_m] as datepart(minute, [start_time]) persisted,
+		[end_h] as datepart(hour, [end_time]) persisted,
+		[end_m] as datepart(minute, [end_time]) persisted,
+
 	constraint [pk_alert_blackouts] primary key clustered 
 	(
-		[alert_id] asc
+		[blackout_id] asc
 	)
 	with (	pad_index = off, 
 			statistics_norecompute = off, 
