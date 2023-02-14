38623b1571edba01660c1132c88bb331028f9b8b
alinselicean <alin.selicean@gmail.com>
2023-02-14 11:36:19 +0200

    a

--- DB/Security/Schemas/ext.sql
+++
@@ -1,3 +0,0 @@
-CREATE SCHEMA [ext]
-AUTHORIZATION [dbo]
-GO
--- DB/Tables/ext.environment.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [ext].[environment]
-(
-[id] [int] NOT NULL IDENTITY(1, 1),
-[name] [nvarchar] (32) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[is_local] [bit] NOT NULL CONSTRAINT [df_environment_is_local] DEFAULT ((0))
-) ON [PRIMARY]
-GO
-ALTER TABLE [ext].[environment] ADD CONSTRAINT [pk_environment] PRIMARY KEY CLUSTERED ([id]) ON [PRIMARY]
-GO
